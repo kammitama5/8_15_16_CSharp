7d4596220f0fd3a90af56bdd549b00304aa2d84a
kammitama5 <krystal.maughan@gmail.com>
2016-08-15 14:48:37 -0700

    first commit

---
+++ FloatingPointNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FloatingPointNumbers
+{
+    class FloatingPointNumbers
+    {
+        /// <summary>
+        /// Demonstrates floating point numbers
+        /// </summary>
+        /// <param name="args"></param>
+        class Program
+        {
+            /// <summary>
+            /// Demonstrates floating point numbers
+            /// </summary>
+            /// <param name="args">command-line args</param>
+            static void Main(string[] args)
+            {
+                // declare variables
+                float score = 1356;
+                int totalSecondsPlayed = 10000;
+
+                // calculate and display result
+                float pointPerSecond = score / totalSecondsPlayed;
+
+                // print output
+                Console.WriteLine("Points per second " + Math.Round(pointPerSecond, 3));
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
