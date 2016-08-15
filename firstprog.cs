1be54e19e6dd48bfac9be6d147f955ea3c2bea54
kammitama5 <krystal.maughan@gmail.com>
2016-08-15 13:24:51 -0700

    first commit

---
+++ firstprog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace firstprog
+{
+
+    /// <summary>
+    /// A class that prints a message 
+    /// </summary>
+    class firstprog
+    {
+        /// <summary>
+        /// Main method to print message
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            //print a message
+            Console.WriteLine("Hi, smart n00b");
+
+            Console.WriteLine();
+        }
+    }
+}
